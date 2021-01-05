3aca8b6e925c7074aed6a96e0beb9483e35f9615
jolesen <stoklund@2pi.dk>
2021-01-05 22:28:31 +0000

    Documentation template from sphinx-quickstart
    FossilOrigin-Name: 9f3c2425df58c4febabdeda26a3278c48e8ba7ca78363fca91847fe00b264140

---
+++ docs/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Path setup --------------------------------------------------------------
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../buildbot_fossil'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'buildbot-fossil'
+copyright = '2021, Jakob Stoklund Olesen'
+author = 'Jakob Stoklund Olesen'
+
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.todo',
+]
+
+templates_path = ['_templates']
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'pyramid'
+html_static_path = ['_static']
+
+
+# -- Extension configuration -------------------------------------------------
+
+# -- Options for todo extension ----------------------------------------------
+
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = True
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. buildbot-fossil documentation master file, created by
+   sphinx-quickstart on Tue Jan  5 14:21:08 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to buildbot-fossil's documentation!
+===========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
